90e5610a8643fb1245df85612ed2dc6d9cf54655
moonk <mkjoehsb@gmail.com>
2022-01-15 01:23:42 +0900

    express response

--- src/server.js
+++ src/server.js
@@ -1,10 +1,20 @@
 import express from "express";
+import res from "express/lib/response";
 
 const PORT=4000;
 const app = express();
 
-const handleHome = () => console.log("Get request is accepted!!");
+const handleHome = (req,res) => {
+    res.send("wawoo!!");
+//    res.end();
+}
+
+const handleLogin = (req,res) => {
+    res.send("Login page welcome");
+}
+
 app.get("/", handleHome);
+app.get("/login", handleLogin);
 const handleListening = () => 
     console.log(`Server listening on port http://localhost:${PORT}`);
 
