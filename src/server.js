0878d5c201f412576eaff4ae3c43673c712c9070
moonk <mkjoehsb@gmail.com>
2022-01-15 01:16:09 +0900

    express request

---
+++ src/server.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const PORT=4000;
+const app = express();
+
+const handleHome = () => console.log("Get request is accepted!!");
+app.get("/", handleHome);
+const handleListening = () => 
+    console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(4000, handleListening);
