eec14d2b0b516b1bce5557d78b288ed0b2043ea5
moonk <mkjoehsb@gmail.com>
2022-01-17 07:20:32 +0900

    how to use variables in pug
    chap5.4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,4 +6,3 @@ export const join = (req,res) => res.send("<h1> Join User</h1>");
 export const login = (req,res) => res.send("<h1> Login User</h1>");
 export const see = (req,res) => res.send("<h1> See User</h1>");
 export const logout = (req,res) => res.send("<h1> Log out User</h1>");
-
--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,8 +1,8 @@
 
 
-export const trending = (req,res) =>  res.render("home");
-export const see = (req,res) => res.render("watch");
-export const edit = (req,res) => res.render("edit");
+export const trending = (req,res) =>  res.render("home",{pageTitle:"home"});
+export const see = (req,res) => res.render("watch",{pageTitle:"watch"});
+export const edit = (req,res) => res.render("edit",{pageTitle:"edit"});
 export const search = (req,res) => res.send("<h1>Search Videos</h1>");
 export const upload = (req,res) => res.send("<h1>Upload Videos</h1>");
 export const deleteVideo = (req,res) => res.send("<h1>Delete Videos</h1>");
