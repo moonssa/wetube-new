58bb4b1deb95d32d6f8904e62cb0159e0b53713b
moonk <mkjoehsb@gmail.com>
2022-02-01 00:24:35 +0900

    add req.session

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -62,7 +62,10 @@ export const postLogin = async (req, res) => {
       errorMessage: "wrong password!!",
     });
   }
-  return res.end();
+  req.session.loggedIn = true;
+  req.session.user = user;
+  
+  return res.redirect("/");
 };
 export const edit = (req, res) => res.send("<h1>Edit User</h1>");
 export const remove = (req, res) => res.send("<h1>Remove User</h1>");
