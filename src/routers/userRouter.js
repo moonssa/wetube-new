5ddf1640f110e0640948461e7e3ca51d74cd57bb
moonk <mkjoehsb@gmail.com>
2022-02-03 22:51:28 +0900

    protector & public middleware
    미들웨어를 이용하여 직접 억세스 하려는 것을 컨트롤한다.

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -9,12 +9,14 @@ import {
   finishGithubLogin,
 } from "../controllers/userController";
 
+import { protectorMiddleware, publicOnlyMiddleware } from "../middlewares";
+
 const userRouter = express.Router();
 userRouter.get("/:id(\\d+)", see);
-userRouter.get("/github/start", startGithubLogin);
-userRouter.get("/github/finish", finishGithubLogin);
-userRouter.route("/edit").get(getEdit).post(postEdit);
+userRouter.get("/github/start", publicOnlyMiddleware, startGithubLogin);
+userRouter.get("/github/finish", publicOnlyMiddleware, finishGithubLogin);
+userRouter.route("/edit").all(protectorMiddleware).get(getEdit).post(postEdit);
 userRouter.get("/remove", remove);
-userRouter.get("/logout", logout);
+userRouter.get("/logout", protectorMiddleware, logout);
 
 export default userRouter;
