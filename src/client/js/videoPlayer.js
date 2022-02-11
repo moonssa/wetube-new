e95353fc7173c2b4873e3c02f7be02de0370993b
moonk <mkjoehsb@gmail.com>
2022-02-12 06:56:48 +0900

    volume control
    muted -> unmuted 될때 이전 볼륨 기억하기

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -4,31 +4,46 @@ const muteBtn = document.getElementById("mute");
 const time = document.getElementById("time");
 const volumeRange = document.getElementById("volume");
 
+video.volume = 0.5;
+let volumeValue =video.volume;
+
 const handlePlayClick = (e) => {
   if (video.paused) {
     video.play();
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused? "Play" : "Pause";
+  playBtn.innerText = video.paused ? "Play" : "Pause";
 };
 
 const handleMuteClick = (e) => {
   if (video.muted) {
     video.muted = false;
+  } else {
+    video.muted = true;
   }
-  else{
-      video.muted = true;
-  }
-  muteBtn.innerText = video.muted? "Unmuted": "Muted";
-  volumeRange.value = video.muted? 0: 0.5;
+  muteBtn.innerText = video.muted ? "Unmuted" : "Muted";
+  volumeRange.value = video.muted ? 0 : volumeValue;
 };
 
+const handleVolumeRange = (event) => {
+  const {
+    target: { value },
+  } = event;
+
+  if(video.muted){
+      video.muted = false;
+      muteBtn.innerText = "Muted";
+  }
+  volumeValue = value;  // memory previous volume
+  video.volume = value;
+};
 //const handlePlay = () => (playBtn.innerText = "Pause");
 //const handlePause = () => (playBtn.innerText = "Play");
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
+volumeRange.addEventListener("input", handleVolumeRange);
 
 //video.addEventListener("play", handlePlay);
 //video.addEventListener("pause", handlePause);
