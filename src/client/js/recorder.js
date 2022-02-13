09431070c136452aa56f2cb702a3890494b018e4
moonk <mkjoehsb@gmail.com>
2022-02-14 08:27:32 +0900

    recording part1

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -1,14 +1,39 @@
 const startBtn = document.getElementById("startBtn");
 const video = document.getElementById("preview");
 
-const handleStartRecord = async () => {
-  console.log("teststtse");
-  const stream = await navigator.mediaDevices.getUserMedia({
-    audio: true,
+let stream;
+
+const handleStop = () => {
+  startBtn.innerText = "Start recording";
+  startBtn.removeEventListener("click", handleStop);
+  startBtn.addEventListener("click", handleStart);
+}
+
+const handleStart = () => {
+  startBtn.innerText = "Stop recording";
+  startBtn.removeEventListener("click", handleStart);
+  startBtn.addEventListener("click",handleStop);
+
+  const recorder = new MediaRecorder(stream);
+  recorder.ondataavailable = (e) => {
+    console.log("recording done");
+    console.log(e);
+    console.log(e.data);
+    startBtn.innerText = "Start recording";
+  };
+  recorder.start();
+  setTimeout(() => {
+    recorder.stop();
+  },10000);
+};
+
+const init = async () => {
+  stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
     video: true,
   });
   video.srcObject = stream;
   video.play();
-};
-
-startBtn.addEventListener("click", handleStartRecord);
+}
+init();
+startBtn.addEventListener("click", handleStart);
