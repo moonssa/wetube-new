ff12def1fa2e90674463a1c441fff9dcf857dd09
moonk <mkjoehsb@gmail.com>
2022-02-27 09:19:52 +0900

    make a comment model
    mongoose 에 저장위한 schema 생성, video, user간에 관계 생성.

--- src/models/Video.js
+++ src/models/Video.js
@@ -8,10 +8,13 @@ export const formatHashtags = (hashtags) =>
 const videoSchema = new mongoose.Schema({
   title: { type: String, required: true, trim: true, maxLength: 30 },
   description: { type: String, required: true, trim: true, minLength: 20 },
-  fileUrl: {type: String, required: true},
-  thumbUrl: {type: String, required: true},
+  fileUrl: { type: String, required: true },
+  thumbUrl: { type: String, required: true },
   createdAt: { type: Date, required: true, default: Date.now },
-  owner: {type: mongoose.Schema.Types.ObjectId, required: true, ref:"User"},
+  comments: [
+    { type: mongoose.Schema.Types.ObjectId, required: true, ref: "Comment" },
+  ],
+  owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
   hashtags: [{ type: String }],
   meta: {
     views: { type: Number, required: true, default: 0 },
@@ -23,7 +26,6 @@ videoSchema.static("formatHashtags", function (hashtags) {
   return hashtags
     .split(",")
     .map((word) => (word.startsWith("#") ? word : `#${word.trim()}`));
-
 });
 
 const Video = mongoose.model("Video", videoSchema);
