e8ca171eaa352481e1d18fd86c76854c7b806b7a
moonk <mkjoehsb@gmail.com>
2022-03-01 11:25:27 +0900

    realtime comment

--- src/client/js/commentSection.js
+++ src/client/js/commentSection.js
@@ -1,25 +1,39 @@
 const videoContainer = document.getElementById("videoContainer");
 const form = document.getElementById("commentForm");
 
-const handleSubmit = async(event) =>{
-    event.preventDefault();
-    const textarea = form.querySelector("textarea");
-    const text = textarea.value;
-    const videoId = videoContainer.dataset.id;
-    console.log(text);
-    if (text === "") {
-        return;
-    }
-    await fetch(`/api/videos/${videoId}/comment`, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({text}),
-    });
-    textarea.value="";
-    window.location.reload();
+const addComment = (text)=> {
+    const videoComments = document.querySelector(".video__comments ul");
+    const newComment = document.createElement("li");
+    newComment.className = "video__comment";
+    const icon = document.createElement("i");
+    icon.className = "fas fa-comment";
+    const span = document.createElement("span");
+    span.innerText = `  ${text}`;
+    newComment.appendChild(icon);
+    newComment.appendChild(span);
+    videoComments.prepend(newComment);
+}
+const handleSubmit = async (event) => {
+  event.preventDefault();
+  const textarea = form.querySelector("textarea");
+  const text = textarea.value;
+  const videoId = videoContainer.dataset.id;
+  console.log(text);
+  if (text === "") {
+    return;
+  }
+  const { status } = await fetch(`/api/videos/${videoId}/comment`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ text }),
+  });
+  textarea.value = "";
+  if(status === 202){
+    addComment(text);
+  }
 };
-if(form){
-    form.addEventListener("submit",handleSubmit);
+if (form) {
+  form.addEventListener("submit", handleSubmit);
 }
