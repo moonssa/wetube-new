8a00037f2a759068553bc609d7677815ad73acc0
moonk <mkjoehsb@gmail.com>
2022-03-04 09:07:12 +0900

    PORT variable add in app.listen

--- src/init.js
+++ src/init.js
@@ -12,4 +12,4 @@ const PORT = process.env.PORT || 4000;
 const handleListening = () => 
     console.log(`✅ Server listening on port http://localhost:${PORT}`);
 
-app.listen(4000, handleListening);
+app.listen(PORT, handleListening);
